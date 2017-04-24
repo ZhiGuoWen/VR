6c4f4cda073349b3bff8ee1cc1a66ea280b415ed
wenzhiguo <wenzhiguo0414@foxmail.com>
2017-04-24 13:29:35 +0800

    mvvm设计模式加载图片

--- mvvmUser/src/main/java/com/wenzhiguo/user/UserMessage.java
+++ mvvmUser/src/main/java/com/wenzhiguo/user/UserMessage.java
@@ -2,6 +2,7 @@ package com.wenzhiguo.user;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 
 /**
  * Created by dell on 2017/4/21.
@@ -15,12 +16,16 @@ public class UserMessage {
     public TextWatcher mWatcher = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+            Log.d("zzz","beforeTextChanged---i   "+i);
+            Log.d("zzz","beforeTextChanged---i1   -"+i1);
+            Log.d("zzz","beforeTextChanged---i2   "+i2);
         }
 
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+            Log.d("zzz","onTextChanged---i   "+i);
+            Log.d("zzz","onTextChanged---i1   "+i1);
+            Log.d("zzz","onTextChanged---i2   "+i2);
         }
 
         @Override
